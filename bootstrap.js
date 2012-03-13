9609c5f354a72893c04c390b76cf5fbc46a685a4
Taras Glek <tglek@mozilla.com>
2012-03-12 19:26:17 -0700

    bump version

--- bootstrap.js
+++ bootstrap.js
@@ -4,7 +4,6 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 const Cm = Components.manager;
-const PREF_ENABLED = "toolkit.telemetry.enabled";
 
 Cm.QueryInterface(Ci.nsIComponentRegistrar);
 
@@ -48,22 +47,7 @@ function appName()
 Cm.QueryInterface(Ci.nsIComponentRegistrar);
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
-const HEIGHT = 18
-function graph(values, max_value) {
-  var html = ""
-  for each ([label, value] in values) {
-    var below = Math.round(HEIGHT * (value / max_value)*10)/10
-    var above = HEIGHT - below
-    html += '<div class="bar">'
-    html += '<div class="above" style="height: ' + above + 'em;"></div>'
-    html += value ? value : "&nbsp;"
-    
-    html += '<div class="below" style="height: ' + below + 'em;"></div>'
-    html += label
-    html += '</div>'
-  }
-  return html
-}
+
 function AboutHistograms() {}
 
 AboutHistograms.prototype = {
@@ -71,98 +55,19 @@ AboutHistograms.prototype = {
   classDescription: "about:telemetry",
   classID: Components.ID("{1b44f837-58e7-4f54-8542-413d13b6d61e}"),
   contractID: "@mozilla.org/network/protocol/about;1?what=telemetry",
-
-  getHTMLTable: function(stats, isMainThread) {
-    if (Object.keys(stats).length == 0) {
-      return "";
-    }
-    var listHtml = '\n<table class=slowSql id="' + (isMainThread ? 'main' : 'other') + 'SqlTable">';
-    listHtml += '\n<caption>Slow SQL Statements on ' + (isMainThread ? 'Main' : 'Other') + ' Thread</caption>';
-    listHtml += '\n<tr><th>Hits</th><th>Avg. Time (ms)</th><th>Statement</th></tr>';
-    for (var key in stats) {
-      var hitCount = stats[key][0];
-      var averageTime = stats[key][1]/hitCount;
-      listHtml += '\n<tr>';
-      listHtml += '<td>' + hitCount + '</td>';
-      listHtml += '<td>' + averageTime.toFixed(0) + '</td>';
-      listHtml += '<td>' + key + '</td>';
-      listHtml += '</tr>';
-    }
-    listHtml += '\n</table>';
-    listHtml += "\n<hr>\n"
-    return listHtml;
-  },
-
+  
   newChannel: function(uri)
   {
     var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
     var html = 'data:text/html,<html><head>' 
                + '<LINK href="' + MY_URL + 'stylesheet.css" rel="stylesheet" type="text/css">'
+               + '<script type="application/javascript;version=1.8" src="' + MY_URL + 'about_telemetry.js"></script>'
                + '</head><body>';
-    var enabled = false;
-    try {
-      enabled = Services.prefs.getBoolPref(PREF_ENABLED);
-    } catch (e) {
-      // Prerequesite prefs aren't set
-    }
-
-    if (enabled) {
-      html += "Telemetry is enabled";
-    } else {
-      html += "Please set "+PREF_ENABLED+" to true in <a href='about:config'>about:config</a>"
-    }
-    //html += '<script src="' + MY_URL + 'brains.js">'
-//    html += '<br><form><input type="text" style="width:50%" value="filter" onchange="doSearch()"></form>'
-    html += "\n<hr>\n"
-    const Telemetry = Cc["@mozilla.org/base/telemetry;1"].getService(Ci.nsITelemetry)
-
-    var h = Telemetry.histogramSnapshots;
-    var s = Telemetry.slowSQL;
-    if (s) {
-      html += this.getHTMLTable(s.mainThread, true);
-      html += this.getHTMLTable(s.otherThreads, false);
-    }
-
-    for (var key in h) {
-      var v = h[key]
-      var sample_count = v.counts.reduceRight(function (a,b)  a+b)
-     
-      var buckets = v.histogram_type == Telemetry.HISTOGRAM_BOOLEAN ? [0,1] : v.ranges;
-      var average =  v.sum / sample_count
-      html += '<div class=main id="'+key+'"><div>'
-      html += key+"<br>" + sample_count + " samples"
-      html += ", average = " + Math.round(average*10)/10
-      html += ", sum = " + v.sum
-      html += "</div>"
-      var max_value = Math.max.apply(Math, v.counts)
-      var first = true
-      var last = 0;
-      var values = []
-      for (var i = 0;i<buckets.length;i++) {
-        var count = v.counts[i]
-        if (!count)
-          continue
-        if (first) {
-          first = true;
-          first = false;
-          if (i) {
-            values.push([buckets[i-1], 0])
-          }
-        }
-        last = i + 1
-        values.push([buckets[i], count])
-      }
-      if (last && last < buckets.length) {
-        values.push([buckets[last],0])
-      }
-      html += graph(values, max_value)
-      html +="</div>\n"
-    }
     html += "</body></html>";
 
     var channel = ioService.newChannel(html, null, null);
     var securityManager = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
-    var principal = securityManager.getCodebasePrincipal(uri);
+    var principal = securityManager.getSystemPrincipal(uri);
     channel.originalURI = uri;
     channel.owner = principal;
     return channel;
@@ -207,8 +112,6 @@ function unMonkeyPatchWindow(w) {
   let menuitem = w.document.getElementById("aboutTelemetryMenuitem");
   menuitem.parentNode.removeChild(menuitem);
 }
-
-//text-overflow:  ellipsis
 
 function startup(aData, aReason) {
   let resource = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
